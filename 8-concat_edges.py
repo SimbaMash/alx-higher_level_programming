bcce93b902caa260dc5da431594350330e779197
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2023-06-05 23:21:39 +0200

    Updating code

--- 8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an intepreted, interactive,object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[37:64] + str[111:117] + str[:6]
-print(str)
